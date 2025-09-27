b8f357fd7a49c821aad202ec480572373c6a098f
iChakiath <151546869+iChakiath@users.noreply.github.com>
2025-09-27 16:05:28 -0400

    Add files via upload

---
+++ CMSC203_Lab1/MovieDriver_Task2.java
@@ -0,0 +1,48 @@
+/*
+ * Class: CMSC203 
+ * Instructor: Professor Khandan Monshi
+ * Description: This program asks the user for a movie, 
+ * the rating of the movie, and the number of tickets sold. 
+ * Then, it displays back the information to the user. The 
+ * program repeats until the user types "n".
+ * Due: 9/30/2025
+ * Platform/compiler: Eclipse
+ * I pledge that I have completed the programming 
+ * assignment independently. I have not copied the code 
+ * from a student or any source. I have not given my code 
+ * to any student.
+   Print your Name here: Iris Chakiath
+*/
+import java.util.Scanner;
+public class MovieDriver_Task2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Movie movie = new Movie();
+		String continueInput;
+		
+		do {
+			//get movie info
+			System.out.println("Enter the name of a movie ");
+			movie.setTitle(scanner.nextLine());
+			System.out.println("Enter the rating of a movie ");
+			movie.setRating(scanner.nextLine());
+			System.out.println("Enter the number of tickets sold for this movie ");
+			movie.setSoldTickets(scanner.nextInt());
+			scanner.nextLine();
+			System.out.println(movie.toString());
+			
+			//continue?
+			System.out.println("Do you want to enter another? (y or n)");
+			continueInput = scanner.nextLine();
+		}while(continueInput.equalsIgnoreCase("y"));
+		
+		//end message
+		System.out.println("Goodbye");
+		
+		
+		scanner.close();
+
+	}
+
+}
